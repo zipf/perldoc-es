d83f4361acdec9c2b9d6027cbf8f41095ebfcaed
zipf <blas.gordon@gmail.com>
2011-03-05 22:03:08 +0100

    Code cleanup

--- tools/postprocessing.pl
+++ tools/postprocessing.pl
@@ -9,6 +9,8 @@ use warnings;
 use File::Copy;
 use File::Basename;
 use Pod::Tidy qw( tidy_files );
+use Text::Wrap qw( wrap $columns );
+use Readonly;
 use utf8;
 
 $|++;
@@ -27,31 +29,11 @@ if ( $ARGV[0] ) {
 
 
 # Get path components
-
 my ($name, $path, $suffix) = fileparse($pod_path, qr{\.pod|\.pm});
 
 
-
-# Wrap lines (OmegaT removes some line breaks) 
-
-if ( $suffix =~ /pod$|pm$/ ) {
-    my $processed = Pod::Tidy::tidy_files(
-                                            files   => [ $pod_path ],
-                                            inplace => 1,
-                                            columns => 80,
-                                         );
-
-}
-
-
-# Add TRANSLATORS section
-
-open my $out, '>>:encoding(latin-1)', $pod_path;
-
-
-if ( $suffix =~ /pod$|pm$/ ) {
-
-    print $out  <<'END';
+# Translators section for POD
+Readonly my $TRANSLATORS_POD => <<'END';
 
 =head1 TRADUCTORES
 
@@ -65,10 +47,8 @@ if ( $suffix =~ /pod$|pm$/ ) {
 
 END
 
-} else {   # e.g., README files
-
-
-    print $out  <<'END';
+#Translators section for READMES
+Readonly my $TRANSLATORS => <<'END';
 
 TRADUCTORES
 
@@ -78,17 +58,69 @@ Enrique Nell, blas.gordon + POD2ES at gmail.com
 
 END
 
+
+
+if ( $suffix =~ /pod$|pm$/ ) {
+
+    # Wrap lines (OmegaT removes some line breaks) using Pod::Tidy 
+    my $processed = Pod::Tidy::tidy_files(
+                                            files   => [ $pod_path ],
+                                            inplace => 1,
+                                            columns => 80,
+                                         );
+
+
+    # Add TRANSLATORS section
+    open my $out, '>>:encoding(latin-1)', $pod_path;
+
+    print $out $TRANSLATORS_POD;
+    
+    close $out;
+
+
+    # Convert pod to html for visual check
+    my $out_html = $path . "/$name.html";
+
+    system("perl -MPod::Simple::HTML -e Pod::Simple::HTML::go $pod_path > $out_html");
+
+
+} else {   # e.g., README files
+
+    # Wrap lines (OmegaT removes some line breaks) using Text::Wrap
+    my ( $in_path, $out_path );
+    $in_path = $pod_path . ".bak";
+    $out_path = $pod_path;
+
+    copy( $pod_path, $in_path );
+
+    open my $in, '<:encoding(latin-1)', $in_path;
+    open my $out, '>:encoding(latin-1)', $out_path;
+
+    $columns = 80;
+
+    while ( <$in> ) {
+
+        if ( /^\s*$/ ) {
+            print $out $_;
+            next;
+        }
+
+        print $out wrap( "", "", ("$_") );
+
+    }
+
+    close $in;
+    
+    # Add TRANSLATORS section 
+    print $out $TRANSLATORS;
+
+    close $out;
+
 }
 
-close $out;
 
 
 
-# Convert pod to html for visual check
-
-my $out_html = $path . "/$name.html";
-
-system("perl -MPod::Simple::HTML -e Pod::Simple::HTML::go $pod_path > $out_html");
 
 
 
