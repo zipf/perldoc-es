8466ba14be452e63ee99c8198759afdee0653bb2
zipf <blas.gordon@gmail.com>
2011-10-07 17:19:25 +0200

    v5.14.2.01

--- POD2-ES/POD2-ES/lib/POD2/ES.pm
+++ POD2-ES/POD2-ES/lib/POD2/ES.pm
@@ -4,26 +4,27 @@ use strict;
 
 use base 'POD2::Base';
 
-our $VERSION = '5.14.1.06';
+our $VERSION = '5.14.2.01';
 
 sub search_perlfunc_re {
     return 'Lista de funciones de Perl en orden alfabético';
 }
 
 sub pod_info {{
-    perl              => '5.14.1',
-    perlbook          => '5.14.1',
-    perlcheat         => '5.14.1',
-    perlfaq1          => '5.14.1',
-    perlfaq2          => '5.14.1',
-    perlintro         => '5.14.1',
-    perlnumber        => '5.14.1',
-    perlsource        => '5.14.1',
-    perlunitut        => '5.14.1',
-    perlutil          => '5.14.1',
-    'README.freebsd'  => '5.14.1',
-    'README.hurd'     => '5.14.1',
-    'README.openbsd'  => '5.14.1',
+    perl              => '5.14.2',
+    perlbook          => '5.14.2',
+    perlcheat         => '5.14.2',
+    perldbmfilter     => '5.14.2',
+    perlfaq1          => '5.14.2',
+    perlfaq2          => '5.14.2',
+    perlintro         => '5.14.2',
+    perlnumber        => '5.14.2',
+    perlsource        => '5.14.2',
+    perlunitut        => '5.14.2',
+    perlutil          => '5.14.2',
+    'README.freebsd'  => '5.14.2',
+    'README.hurd'     => '5.14.2',
+    'README.openbsd'  => '5.14.2',
 }};
 
 sub print_pod {
