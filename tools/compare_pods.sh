115607f78bc7a06f5f945186ae63cc565c407abb
zipf <blas.gordon@gmail.com>
2012-09-04 19:28:14 +0200

    Fixed messages, typos

--- tools/compare_pods.sh
+++ tools/compare_pods.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 #
-# Compare versions of pods documents.
+# Compare versions of pod documents.
 # Joaquin Ferrero. 2012.07.25
 #
-# Given a pod filename, this program
-# get it form source/ and target/ directories,
-# convert them to manual pages (with perldoc)
-# and show the diffs face to face.
-#
+# Given a pod filename, this program gets the versions 
+# contained in source/ and target/ directories,
+# converts them to manual pages (using perldoc)
+# and shows diffs side-by-side.
+
 
 COLS=$(tput cols)
 POD=$1
@@ -16,18 +16,18 @@ test -n "$POD"                             || exit;
 cd ~/perlspanish                           || exit
 test -f source/$POD && test -f target/$POD || exit;
 
-# Formating pod with MANWIDTH width
+# Format pod with MANWIDTH width
 MEDIO=$[$COLS/2-5]
 MANWIDTH=$MEDIO perldoc -d $POD.en source/$POD
 
-# This encoding line is necessary because target/ pods
-# don't pass by postprocess program, yet
+# This encoding line is required because target/ pods
+# don't go through the postprocess program, yet
 echo -e "=encoding utf-8\n\n" > $POD.es.org
 cat target/$POD >> $POD.es.org
 MANWIDTH=$MEDIO perldoc -d $POD.es $POD.es.org
 
-# Show face to face
+# Show side-by-side
 diff -y -W $COLS $POD.{en,es} |less
 
-# Fin: Delete temporal files
+# Delete temporal files
 rm $POD.{en,es,es.org}
