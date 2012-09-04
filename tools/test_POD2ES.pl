e9525794333712cd1b72fc99af3bf35a78a9cbc0
zipf <blas.gordon@gmail.com>
2012-09-04 19:34:21 +0200

    Fixed comments, messages

--- tools/test_POD2ES.pl
+++ tools/test_POD2ES.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/env perl
 #
-# Checl POD2::ES instalation
+# Check POD2::ES setup
 #
 use strict;
 use warnings;
@@ -11,10 +11,10 @@ use POD2::ES;
 
 my $pod_es = POD2::ES->new();
 
-say 'Instalation directories';
+say 'Setup directories';
 say "\t", join("\n\t", $pod_es->pod_dirs()), "\n";
 
-say 'Title of Alphabetic list of Perl functions:';
+say 'Title of alphabetic list of Perl functions:';
 my $text = $pod_es->search_perlfunc_re();
 say "\t$text\n";
 
@@ -22,6 +22,6 @@ say "List of translated pods:";
 $pod_es->print_pods();
 say '';
 
-say 'Status of perlintro:';
+say 'perlintro status:';
 $pod_es->print_pod('perlintro');
 say '';
