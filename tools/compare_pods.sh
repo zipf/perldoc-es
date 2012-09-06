89536c6c4b84ff1562fe9215f30f7f349e9561ba
joaquinferrero <explorer+git@joaquinferrero.com>
2012-09-07 01:02:49 +0200

    More comments

--- tools/compare_pods.sh
+++ tools/compare_pods.sh
@@ -7,27 +7,39 @@
 # contained in source/ and target/ directories,
 # converts them to manual pages (using perldoc)
 # and shows diffs side-by-side.
+#
+# Run:
+#	compare_pods.sh perl.pod
+#
 
+### Config
+# Project directory
+DIR=/home/explorer/perlspanish
+### End config
 
-COLS=$(tput cols)
+### Arguments
 POD=$1
 
-test -n "$POD"                             || exit;
-cd ~/perlspanish                           || exit
-test -f source/$POD && test -f target/$POD || exit;
+### Constants
+COLS=$(tput cols)
 
-# Format pod with MANWIDTH width
+### Checks
+test -n "$POD"					|| exit 1
+cd $DIR						|| exit 2
+test -f source/$POD && test -f target/$POD	|| exit 3
+
+### Format pod with MANWIDTH width
 MEDIO=$[$COLS/2-5]
 MANWIDTH=$MEDIO perldoc -d $POD.en source/$POD
 
-# This encoding line is required because target/ pods
+### This encoding line is required because target/ pods
 # don't go through the postprocess program, yet
 echo -e "=encoding utf-8\n\n" > $POD.es.org
 cat target/$POD >> $POD.es.org
 MANWIDTH=$MEDIO perldoc -d $POD.es $POD.es.org
 
-# Show side-by-side
+### Show side-by-side
 diff -y -W $COLS $POD.{en,es} |less
 
-# Delete temporal files
-rm $POD.{en,es,es.org}
+### Delete temporal files
+rm -f $POD.{en,es,es.org}
