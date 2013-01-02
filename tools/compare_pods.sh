6aaafee29951f718d7360c3d1825553f856eef97
joaquinferrero <explorer+git@joaquinferrero.com>
2013-01-02 22:27:51 +0100

    v5.16.2.02

--- tools/compare_pods.sh
+++ tools/compare_pods.sh
@@ -20,13 +20,22 @@ DIR=/home/explorer/perlspanish
 ### Arguments
 POD=$1
 
+if [ -f $POD ]
+then
+	POD_SOURCE=$POD
+	POD_TARGET=$2
+else
+	POD_SOURCE="source/$POD"
+	POD_TARGET="target/$POD"
+fi
+
 ### Constants
 COLS=$(tput cols)
 
 ### Checks
 test -n "$POD"					|| exit 1
 cd $DIR						|| exit 2
-test -f source/$POD && test -f target/$POD	|| exit 3
+test -f $POD_SOURCE && test -f $POD_TARGET	|| exit 3
 
 ## Middle-point of screen
 MEDIO=$[$COLS/2]
@@ -34,19 +43,19 @@ MEDIO=$[$COLS/2]
 W_EN=$[$MEDIO*95/100]
 W_ES=$[$MEDIO*100/100]
 
-echo "Columnas: $COLS"
-echo "Medio:    $MEDIO"
-echo "EN:       $W_EN"
-echo "ES:       $W_ES"
+#echo "Columnas: $COLS"
+#echo "Medio:    $MEDIO"
+#echo "EN:       $W_EN"
+#echo "ES:       $W_ES"
 #exit
 
 ### Format pod with MANWIDTH width
-MANWIDTH=$W_EN PERLDOC_POD2="" perldoc -d $POD.en source/$POD
+MANWIDTH=$W_EN PERLDOC_POD2="" perldoc -d $POD.en $POD_SOURCE
 
 ### This encoding line is required because target/ pods
 # don't go through the postprocess program, yet
 echo -e "=encoding utf-8\n\n" > $POD.es.org
-cat target/$POD >> $POD.es.org
+cat $POD_TARGET >> $POD.es.org
 MANWIDTH=$W_ES PERLDOC_POD2="" perldoc -d $POD.es $POD.es.org
 
 ### Show side-by-side
